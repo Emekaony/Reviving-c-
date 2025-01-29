ef37c1fdc25c38739fa744c9496dbd6ed0bab029
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-01-29 01:06:49 -0800

    Easy money

--- pacman-rules/pacman_rules.cpp
+++ pacman-rules/pacman_rules.cpp
@@ -9,22 +9,19 @@ bool can_eat_ghost(bool power_pellet_active, bool touching_ghost) {
 // The function should return true if Pac-Man is touching a power pellet or a
 // dot.
 bool scored(bool touching_power_pellet, bool touching_dot) {
-    // TODO: Please implement the scored function
-    return false;
+    return touching_power_pellet || touching_dot;
 }
 
 // lost returns a boolean value if Pac-Man loses.
 // The function should return true if Pac-Man is touching a ghost and
 // does not have a power pellet active.
 bool lost(bool power_pellet_active, bool touching_ghost) {
-    // TODO: Please implement the lost function
-    return false;
+    return touching_ghost && !power_pellet_active;
 }
 
 // won returns a boolean value if Pac-Man wins.
 // The function should return true if Pac-Man
 // has eaten all of the dots and has not lost
 bool won(bool has_eaten_all_dots, bool power_pellet_active, bool touching_ghost) {
-    // TODO: Please implement the won function
-    return false;
+    return has_eaten_all_dots && !lost(power_pellet_active, touching_ghost);
 }
