e5baff8ba1a90ee2bf5c6e71844f03b480a61ffa
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-01-29 13:04:09 -0800

    Discount should be applied to the daily rate not the budget

--- freelancer-rates/freelancer_rates.cpp
+++ freelancer-rates/freelancer_rates.cpp
@@ -1,32 +1,33 @@
 // INFO: Headers from the standard library should be inserted at the top via
-// #include <LIBRARY_NAME>
+#include <cmath>
 
+const float DAILY_BILLABLE_HOURS = 8.0;
+const float MONTHLY_BILLABLE_DAYS = 22.0;
 // daily_rate calculates the daily rate given an hourly rate
 double daily_rate(double hourly_rate) {
-    // TODO: Implement a function to calculate the daily rate given an hourly
-    // rate
-    return 0.0;
+    return hourly_rate * DAILY_BILLABLE_HOURS;
 }
 
 // apply_discount calculates the price after a discount
 double apply_discount(double before_discount, double discount) {
-    // TODO: Implement a function to calculate the price after a discount.
-    return 0.0;
+    return before_discount - before_discount * (discount / 100);
 }
 
 // monthly_rate calculates the monthly rate, given an hourly rate and a discount
 // The returned monthly rate is rounded up to the nearest integer.
-int monthly_rate(double hourly_rate, double discount) {
-    // TODO: Implement a function to calculate the monthly rate, and apply a
-    // discount.
-    return 0;
+double monthly_rate(double hourly_rate, double discount) {
+    double before_discount = daily_rate(hourly_rate) * MONTHLY_BILLABLE_DAYS;
+    return std::ceil(apply_discount(before_discount, discount));
 }
 
 // days_in_budget calculates the number of workdays given a budget, hourly rate,
 // and discount The returned number of days is rounded down (take the floor) to
 // the next integer.
 int days_in_budget(int budget, double hourly_rate, double discount) {
-    // TODO: Implement a function that takes a budget, an hourly rate, and a
-    // discount, and calculates how many complete days of work that covers.
-    return 0;
+    // apply the discount on the daily rate not the budget!!
+    // we're doing them a favor, budget is fixed
+    double discounted_daily_rate =
+        apply_discount(daily_rate(hourly_rate), discount);
+
+    return std::floor(budget / discounted_daily_rate);
 }
