d1c663932a2309ccdc8256fb9fcab7dbf19735e9
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-01-31 01:28:55 -0800

    used < so we account for the first years interest

--- interest-is-interesting/interest_is_interesting.cpp
+++ interest-is-interesting/interest_is_interesting.cpp
@@ -1,25 +1,49 @@
+
+/*
+
+    3.213% for a balance less than 0 dollars (balance gets more negative).
+    0.5% for a balance greater than or equal to 0 dollars, and less than 1000
+   dollars.
+   1.621% for a balance greater than or equal to 1000 dollars, and less
+   than 5000 dollars.
+   2.475% for a balance greater than or equal to 5000
+   dollars.
+
+*/
+
 // interest_rate returns the interest rate for the provided balance.
 double interest_rate(double balance) {
-    // TODO: Implement the interest_rate function
-    return 0.0;
+    double rate{0.0};
+    if (balance < 0) {
+        rate = 3.213;
+    } else if (balance >= 0 && balance < 1000) {
+        rate = 0.5;
+    } else if (balance >= 1000 && balance < 5000) {
+        rate = 1.621;
+    } else if (balance >= 5000) {
+        rate = 2.475;
+    }
+    return rate;
 }
 
 // yearly_interest calculates the yearly interest for the provided balance.
 double yearly_interest(double balance) {
-    // TODO: Implement the yearly_interest function
-    return 0.0;
+    return interest_rate(balance) * balance / 100;
 }
 
 // annual_balance_update calculates the annual balance update, taking into
 // account the interest rate.
 double annual_balance_update(double balance) {
-    // TODO: Implement the annual_balance_update function
-    return 0.0;
+    return yearly_interest(balance) + balance;
 }
 
 // years_until_desired_balance calculates the minimum number of years required
 // to reach the desired balance.
 int years_until_desired_balance(double balance, double target_balance) {
-    // TODO: Implement the years_until_desired_balance function
-    return 0;
+    int cnt{0};
+    while (balance < target_balance) {
+        cnt++;
+        balance = annual_balance_update(balance);
+    }
+    return cnt;
 }
